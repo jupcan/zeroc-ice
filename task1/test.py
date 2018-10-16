820f6b2284e3e7e0b5215d9556a2e294ce2fe923
jupcan <jpereac@gmail.com>
2018-10-16 16:40:44 +0200

    task2 files

--- task1/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/prego
-# -*- mode:python; coding:utf-8 -*-
-
-from _test import ClientServerMixin
-
-
-class Hello(ClientServerMixin):
-    def test_client_server(self):
-        self.make_client_server('./Client.py', './Server.py')
